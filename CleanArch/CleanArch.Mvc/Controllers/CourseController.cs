61a2bc5d0c8601e7b4ffac1f3c55dc646202c197
Santhosh <sdwarakanath@outlook.com>
2020-07-08 03:02:32 -0700

    Added course controller, view and styles

---
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel courseViewModel = _courseService.GetCourses();
+            return View(courseViewModel);
+        }
+    }
+}
